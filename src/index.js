64368ed426bd91ce6c6d6e765cd514372b7d5ae2
ramzirifqah <muhammadramzirifqah@gmail.com>
2025-10-06 13:14:50 +0700

    add method post

--- src/index.js
+++ src/index.js
@@ -1,9 +1,29 @@
 import express from "express"
 
 const app = express();
+app.use(express.json());
+
+//Dummy data
+let users = [
+    { id: 1, name:"bane"},
+    { id: 2, name:"ramzi"},
+];
+
+//method get
+app.get("/users", (req,res) => {
+    res.json(users)
+});
 
 app.get("/tes", (req,res) => {
-    res.send("Welcome bray...")
+    res.json({msg:"Welcome bray...",code: 200})
+});
+
+//method post
+app.post("/users",(req,res) => {
+    const{name} = req.body;
+    const newUser = { id: users.length + 1, name: name};
+    users.push(newUser)
+    res.status(201).json(newUser);
 });
 
 app.listen(5001, () => console.log("Server running on http://localhost:5001"));
